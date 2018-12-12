00c0b794c18a6937dcba58b86ca44e85cd774ef6
mksbnt <p38o68@gmail.com>
2018-12-12 18:52:41 +0200

    complete task1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminComponent }       from './admin/admin.component';
+import { HomeComponent }        from './home/home.component';
+import { ProductsComponent }    from './products/products.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
